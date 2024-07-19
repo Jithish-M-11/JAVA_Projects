a95b8a2cf6cc47f63113c78980842c192a2bab2c
Jithish-M-11 <jithishmohan123@gmail.com>
2024-07-20 01:50:00 +0530

    Creating a busReservation using java

---
+++ BUS_RESERVATION/Booking.java
@@ -0,0 +1,7 @@
+package BUS_RESERVATION;
+import java.util.*;
+public class Booking {
+    String passengername;
+    int busno;
+    //Date date;
+}
---
+++ BUS_RESERVATION/Bus.java
@@ -0,0 +1,9 @@
+package BUS_RESERVATION;
+import java.util.*;
+public class Bus {
+    int busno;
+    String AC="";
+    int capacity ;
+    
+    
+}
